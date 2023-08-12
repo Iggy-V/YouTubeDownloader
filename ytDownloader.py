44349f3ce8f69cce1fa82bb39d34630181fb8142
Iggy-V <ignasvolcokas@gmail.com>
2023-08-12 19:25:10 +0300

    fixed some issues, added try and except

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,21 +1,29 @@
 from pytube import YouTube
+from pathlib import Path
+default_path = str(Path.home() / "Downloads")
 
-def download_vid(link, folder):
+def download_vid(link, folder=default_path):
     """
     link - a web link, should be a YouTube link
     folder - a folder to which to download
     """
+    # print(link)
+    # if "https://www.youtube.com/" not in link and link != "https://www.youtube.com":
+    #     print("Not a Youtube Video Link")
+    #     return
+    try:
+        yt = YouTube(link)
+        yd = yt.streams.get_highest_resolution()
+        yd.download(folder)
+    except:
+        pass
+    
 
-    if "https://www.youtube.com/" not in link:
-        print("Not a Youtube Link")
-        return
-    yt = YouTube(link)
 
     #print("title: ", yt.title)
 
-    yd = yt.streams.get_highest_resolution()
-    yd.download(folder)
 
-    
-
+if __name__ == "__main__":
+    link = "https://www.youtube.com/watch?v=yWU8ynQ_tKs"
+    download_vid(link)
 
