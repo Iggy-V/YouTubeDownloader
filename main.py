3f9dbc9e2ae829db73db4dd66cf87412393f3ea0
Iggy-V <ignasvolcokas@gmail.com>
2023-08-12 19:24:57 +0300

    added main script to run the process

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 """
 Main script to run and download all opened Youtube tabs
+It downloads them to the default download folder
 """
+import ytDownloader
+import getLinks
+
+def main():
+    url_list = getLinks.get_chrome_tabs()
+    for link in url_list:
+        ytDownloader.download_vid(link)
+    
+    print("Done")
+
+if __name__ == "__main__":
+    main()
